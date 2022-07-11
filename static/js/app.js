83b413c11e96055ec1de8073e0191c60370e2924
BeitxuelaWEB <bearobledillogomez@gmail.com>
2022-07-11 11:49:05 +0200

    add js

---
+++ static/js/app.js
@@ -0,0 +1,18 @@
+import API from './api.js'
+import Character from './character.js'
+const api = new API()
+let currentCaracter = 4
+const $loadNext = document.querySelector('#load-next')
+
+$loadNext.addEventListener('click', async () => {
+    const characterData = await api.getCharacter(++currentCaracter)
+    new Character(characterData)
+})
+
+async function initApp(initCharacterId) {
+    const characterData = await api.getCharacter(initCharacterId)
+    console.log(characterData)
+    new Character(characterData)
+}
+
+initApp(currentCaracter)
